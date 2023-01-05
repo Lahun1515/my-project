c196388078a11bd1195d829ac46a9db04c654236
Lahun1515 <121691422+Lahun1515@users.noreply.github.com>
2023-01-05 01:32:53 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+let a= 9;
+let b=10;
+let result= a+b;
+alert(a+b)
+
+myFunction= name => alert("Thank you for successfully creating an account with us " + name);
+ prompt(" what is your name");
+ myFunction= name=> alert(" what is your age " + name );
